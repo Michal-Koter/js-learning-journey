fc3b6eeca4855bb581a2b730188c0f85570f5f1b
Michal-Koter <kotermichal@outlook.com>
2026-02-25 20:21:25 +0100

    add babel and mini css extract plugin to webpack

--- webpack-starter/webpack.config.js
+++ webpack-starter/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     mode: "development",
@@ -22,7 +23,17 @@ module.exports = {
         rules: [
             {
                 test: /\.css$/,
-                use: ["style-loader", "css-loader"]
+                use: [MiniCssExtractPlugin.loader, "css-loader"]
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env"]
+                    }
+                }
             }
         ]
     },
@@ -31,6 +42,7 @@ module.exports = {
             title: "Webpack App",
             filename: "index.html",
             template: "./src/index.html"
-        })
+        }),
+        new MiniCssExtractPlugin()
     ]
 }
